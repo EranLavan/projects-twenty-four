6e88aaa25679804c138addefa2055c4ae355598f
Grigorii Belov <belovgrigory139@gmail.com>
2024-07-01 22:41:24 +0300

    modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Milim from './Milim/Milim.js'
 import Tests from './Milim/Tests.js'
 
 function App() {
   return (
-    <Router basename={process.env.PUBLIC_URL}>
+    <Router>
       <Routes>
         
           <Route path="/" element={<Milim />} />
