454d9aedf91030c924773595b2358dfc17396a81
Grigorii Belov <belovgrigory139@gmail.com>
2024-07-04 19:54:25 +0300

    modified:   src/App.js 	modified:   src/Milim/Tests.js 	new file:   src/Quad/Quad.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Milim from './Milim/Milim.js'
 import Tests from './Milim/Tests.js'
+import Quad from './Quad/Quad.js'
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         
           <Route path="/" element={<Milim />} />
           <Route path="/tests" element={<Tests />} />
+          <Route path="/quad" element={<Quad />} />
         
       </Routes>
     </Router>
---
+++ src/Quad/Quad.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react'
+
+function Quad() {
+
+
+
+    
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Quad
