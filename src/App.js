73e96420aebf923274792fa96e9dc26f1c4f4964
Grigorii Belov <belovgrigory139@gmail.com>
2024-07-06 18:53:47 +0300

    modified:   src/App.js 	new file:   src/Edge/Edge.js 	new file:   src/Main/Main.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
 import React from 'react';
 import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
-import Milim from './Milim/Milim.js'
-import Tests from './Milim/Tests.js'
-import Quad from './Quad/Quad.js'
-import Solubility from './Solubility/SolubilityDraft.js'
+import Main from './Main/Main.js';
+import Milim from './Milim/Milim.js';
+import Tests from './Milim/Tests.js';
+import Quad from './Quad/Quad.js';
+import Solubility from './Solubility/SolubilityDraft.js';
 import RandomizerA from './Randomizer/RandomizerA.js';
+import Edge from './Edge/Edge.js';
 
 function App() {
   return (
     <Router>
       <Routes>
         
-          <Route path="/" element={<Milim />} />
+          <Route path="/" element={<Main />} />
+          <Route path="/Milim" element={<Milim />} />
           <Route path="/tests" element={<Tests />} />
           <Route path="/quad" element={<Quad />} />
           <Route path="/solubility" element={<Solubility />} />
           <Route path="/randomizer" element={<RandomizerA />} />
+          <Route path="/edge" element={<Edge />} />
         
       </Routes>
     </Router>
