0d33d4e8bb90d130cda4fef0c43d0a31b338720a
Grigorii Belov <belovgrigory139@gmail.com>
2024-07-10 18:54:34 +0300

    modified:   src/Edge/NewBrunswick.js 	modified:   src/Edge/Provinces.module.css 	modified:   src/Main/Main.js

--- src/Edge/NewBrunswick.js
+++ src/Edge/NewBrunswick.js
@@ -10,39 +10,51 @@ function NewBrunswick() {
 
       <div className={styles.innerContainer}>
         <div className={styles.linkpic}>
-          <a href=''>
-            <img className={styles.nb1} src='' alt='New Brunswick 1'></img>
+          <a href='https://images.unsplash.com/photo-1649803153066-ebf1a3ced231?q=80&w=1477&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'>
+            <img className={styles.nb1} src='https://images.unsplash.com/photo-1649803153066-ebf1a3ced231?q=80&w=1477&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' alt='New Brunswick 1'></img>
           </a>
+          <br />
+          Photo by <a href="https://unsplash.com/@eprouzet?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Eric Prouzet</a> on <a href="https://unsplash.com/photos/a-large-body-of-water-under-a-cloudy-sky-RjCyCurw82U?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
         </div>
 
         <div className={styles.linkpic}>
-          <a href=''>
-            <img className={styles.nb2} src='' alt='New Brunswick 2'></img>
+          <a href='https://images.unsplash.com/photo-1590275643728-07a266bdace2?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'>
+            <img className={styles.nb2} src='https://images.unsplash.com/photo-1590275643728-07a266bdace2?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' alt='New Brunswick 2'></img>
           </a>
+          <br />
+          Photo by <a href="https://unsplash.com/@conorhennessy_?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Conor Hennessy</a> on <a href="https://unsplash.com/photos/green-and-brown-trees-under-white-clouds-during-daytime-ROlXRfdhmEo?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
         </div>
 
         <div className={styles.linkpic}>
-          <a href=''>
-            <img className={styles.nb3} src='' alt='New Brunswick 3'></img>
+          <a href='https://images.unsplash.com/photo-1536014972440-62c94be11346?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'>
+            <img className={styles.nb3} src='https://images.unsplash.com/photo-1536014972440-62c94be11346?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' alt='New Brunswick 3'></img>
           </a>
+          <br />
+          Photo by <a href="https://unsplash.com/@enriquekq?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Enrique Quiroga</a> on <a href="https://unsplash.com/photos/pathway-in-between-trees-iYFd50U8Ojw?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
         </div>
 
         <div className={styles.linkpic}>
-          <a href=''>
-            <img className={styles.nb4} src='' alt='New Brunswick 4'></img>
+          <a href='https://images.unsplash.com/photo-1571862729121-3c9e75b95c97?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'>
+            <img className={styles.nb4} src='https://images.unsplash.com/photo-1571862729121-3c9e75b95c97?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' alt='New Brunswick 4'></img>
           </a>
+          <br />
+          Photo by <a href="https://unsplash.com/@nathanielpitcher?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Nathaniel Pitcher</a> on <a href="https://unsplash.com/photos/body-of-water-surrounded-by-trees-RH5jXMYh77U?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
         </div>
 
         <div className={styles.linkpic}>
-          <a href=''>
-            <img className={styles.nb5} src='' alt='New Brunswick 5'></img>
+          <a href='https://images.unsplash.com/photo-1574581883631-668bf645085f?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'>
+            <img className={styles.nb5} src='https://images.unsplash.com/photo-1574581883631-668bf645085f?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' alt='New Brunswick 5'></img>
           </a>
+          <br />
+          Photo by <a href="https://unsplash.com/@lesargonautes?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Les Argonautes</a> on <a href="https://unsplash.com/photos/structures-near-trees-FhSJZQRrvgE?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
         </div>
 
         <div className={styles.linkpic}>
-          <a href=''>
-            <img className={styles.nb6} src='' alt='New Brunswick 6'></img>
+          <a href='https://images.unsplash.com/photo-1653660490141-a72b9e4107f5?q=80&w=1528&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'>
+            <img className={styles.nb6} src='https://images.unsplash.com/photo-1653660490141-a72b9e4107f5?q=80&w=1528&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' alt='New Brunswick 6'></img>
           </a>
+          <br />
+          Photo by <a href="https://unsplash.com/@nickanthony?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Nick Martin</a> on <a href="https://unsplash.com/photos/a-body-of-water-sitting-next-to-a-lush-green-hillside-mUpTxazmfbs?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
         </div>
 
 
--- src/Main/Main.js
+++ src/Main/Main.js
@@ -3,7 +3,15 @@ import React from 'react'
 function Main() {
   return (
     <div>
-      
+      Here's the main page
+      <br />
+
+      <a href='./#/milim'>Milim</a><br />
+      <a href='./#/solubility'>Solubility Table</a><br />
+      <a href='./#/quad'>Quadratic Equation root calculator</a><br />
+      <a href='./#/randomizer'>Randomizer</a><br />
+      <a href='./#/edge'>On the Edge of the World</a><br />
+      <a href='./#/calendar'>Calendar</a><br />
     </div>
   )
 }
