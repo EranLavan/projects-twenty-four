f72ac02d8edf559495e04931f19ba1b3c006eeef
Grigorii Belov <belovgrigory139@gmail.com>
2024-07-10 23:31:19 +0300

    modified:   src/Edge/NovaScotia.js 	modified:   src/Edge/Nunavut.js 	modified:   src/Edge/Provinces.module.css 	modified:   src/Milim/Milim.module.css

--- src/Edge/NovaScotia.js
+++ src/Edge/NovaScotia.js
@@ -10,39 +10,51 @@ function NovaScotia() {
 
       <div className={styles.innerContainer}>
         <div className={styles.linkpic}>
-          <a href=''>
-            <img className={styles.novasc1} src='' alt='Nova Scotia 1'></img>
+          <a href='https://images.unsplash.com/photo-1605810326450-2d391235384b?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'>
+            <img className={styles.novasc1} src='https://images.unsplash.com/photo-1605810326450-2d391235384b?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' alt='Nova Scotia 1'></img>
           </a>
+          <br />
+          Photo by <a href="https://unsplash.com/@lizcummings?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Liz Cummings</a> on <a href="https://unsplash.com/photos/leafless-tree-near-body-of-water-during-daytime-bhfjSK-hLkQ?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
         </div>
 
         <div className={styles.linkpic}>
-          <a href=''>
-            <img className={styles.novasc2} src='' alt='Nova Scotia 2'></img>
+          <a href='https://images.unsplash.com/photo-1635609163186-a68bfa590438?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'>
+            <img className={styles.novasc2} src='https://images.unsplash.com/photo-1635609163186-a68bfa590438?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' alt='Nova Scotia 2'></img>
           </a>
+          <br />
+          Photo by <a href="https://unsplash.com/@janwalterluigi?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Jan Walter Luigi</a> on <a href="https://unsplash.com/photos/a-scenic-view-of-a-forest-with-a-lake-in-the-foreground-7t4sLKNwTfw?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
         </div>
 
         <div className={styles.linkpic}>
-          <a href=''>
-            <img className={styles.novasc3} src='' alt='Nova Scotia 3'></img>
+          <a href='https://images.unsplash.com/photo-1497900174279-304a787fa563?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'>
+            <img className={styles.novasc3} src='https://images.unsplash.com/photo-1497900174279-304a787fa563?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' alt='Nova Scotia 3'></img>
           </a>
+          <br />
+          Photo by <a href="https://unsplash.com/@timberfoster?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Tim Foster</a> on <a href="https://unsplash.com/photos/green-trees-on-grassland-kSJMF4AKyO0?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
         </div>
 
         <div className={styles.linkpic}>
-          <a href=''>
-            <img className={styles.novasc4} src='' alt='Nova Scotia 4'></img>
+          <a href='https://images.unsplash.com/photo-1647179307653-97a662a73fcc?q=80&w=1472&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'>
+            <img className={styles.novasc4} src='https://images.unsplash.com/photo-1647179307653-97a662a73fcc?q=80&w=1472&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' alt='Nova Scotia 4'></img>
           </a>
+          <br />
+          Photo by <a href="https://unsplash.com/@lightblitz?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Justin Wei</a> on <a href="https://unsplash.com/photos/a-scenic-view-of-the-ocean-and-a-road-GHcFgHyVY3s?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
         </div>
 
         <div className={styles.linkpic}>
-          <a href=''>
-            <img className={styles.novasc5} src='' alt='Nova Scotia 5'></img>
+          <a href='https://images.unsplash.com/photo-1598957130949-385db2b60ba3?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'>
+            <img className={styles.novasc5} src='https://images.unsplash.com/photo-1598957130949-385db2b60ba3?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' alt='Nova Scotia 5'></img>
           </a>
+          <br />
+          Photo by <a href="https://unsplash.com/@lizcummings?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Liz Cummings</a> on <a href="https://unsplash.com/photos/brown-wooden-bridge-on-sea-shore-during-daytime--tXphB-vhRM?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
         </div>
 
         <div className={styles.linkpic}>
-          <a href=''>
-            <img className={styles.novasc6} src='' alt='Nova Scotia 6'></img>
+          <a href='https://images.unsplash.com/photo-1591646878704-bd9752df286e?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'>
+            <img className={styles.novasc6} src='https://images.unsplash.com/photo-1591646878704-bd9752df286e?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' alt='Nova Scotia 6'></img>
           </a>
+          <br />
+          Photo by <a href="https://unsplash.com/@sharissajohnson?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Sharissa Johnson</a> on <a href="https://unsplash.com/photos/brown-wooden-dock-on-body-of-water-during-daytime-snFTgztCXzA?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
         </div>
 
 
--- src/Edge/Nunavut.js
+++ src/Edge/Nunavut.js
@@ -10,39 +10,51 @@ function Nunavut() {
 
       <div className={styles.innerContainer}>
         <div className={styles.linkpic}>
-          <a href=''>
-            <img className={styles.nunavut1} src='' alt='Nunavut 1'></img>
+          <a href='https://images.unsplash.com/photo-1613238717410-1b41f0458e47?q=80&w=1378&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'>
+            <img className={styles.nunavut1} src='https://images.unsplash.com/photo-1613238717410-1b41f0458e47?q=80&w=1378&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' alt='Nunavut 1'></img>
           </a>
+          <br />
+          Photo by Isaac Demeester on Unsplash
         </div>
 
         <div className={styles.linkpic}>
-          <a href=''>
-            <img className={styles.nunavut2} src='' alt='Nunavut 2'></img>
+          <a href='https://images.unsplash.com/photo-1713248491040-7dd4a4063d1f?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'>
+            <img className={styles.nunavut2} src='https://images.unsplash.com/photo-1713248491040-7dd4a4063d1f?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' alt='Nunavut 2'></img>
           </a>
+          <br />
+          Photo by Cam Stockdale on Unsplash
         </div>
 
         <div className={styles.linkpic}>
-          <a href=''>
-            <img className={styles.nunavut3} src='' alt='Nunavut 3'></img>
+          <a href='https://images.unsplash.com/photo-1611877466031-f092b2e92fff?q=80&w=1377&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'>
+            <img className={styles.nunavut3} src='https://images.unsplash.com/photo-1611877466031-f092b2e92fff?q=80&w=1377&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' alt='Nunavut 3'></img>
           </a>
+          <br />
+          Photo by Isaac Demeester on Unsplash
         </div>
 
         <div className={styles.linkpic}>
-          <a href=''>
-            <img className={styles.nunavut4} src='' alt='Nunavut 4'></img>
+          <a href='https://images.unsplash.com/photo-1570669405132-f4771bdf399a?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'>
+            <img className={styles.nunavut4} src='https://images.unsplash.com/photo-1570669405132-f4771bdf399a?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' alt='Nunavut 4'></img>
           </a>
+          <br />
+          Photo by Isaac Demeester on Unsplash          
         </div>
 
         <div className={styles.linkpic}>
-          <a href=''>
-            <img className={styles.nunavut5} src='' alt='Nunavut 5'></img>
+          <a href='https://images.unsplash.com/photo-1615508136643-42732af9f1a9?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'>
+            <img className={styles.nunavut5} src='https://images.unsplash.com/photo-1615508136643-42732af9f1a9?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' alt='Nunavut 5'></img>
           </a>
+          <br />
+          Photo by Isaac Demeester on Unsplash
         </div>
 
         <div className={styles.linkpic}>
-          <a href=''>
-            <img className={styles.nunavut6} src='' alt='Nunavut 6'></img>
+          <a href='https://images.unsplash.com/photo-1594443264769-88a15d56fa29?q=80&w=1631&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'>
+            <img className={styles.nunavut6} src='https://images.unsplash.com/photo-1594443264769-88a15d56fa29?q=80&w=1631&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' alt='Nunavut 6'></img>
           </a>
+          <br />
+          Photo by Damon on Road on Unsplash
         </div>
 
 
